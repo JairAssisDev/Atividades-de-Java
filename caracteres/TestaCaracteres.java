11c7a480534e22ae3ea538d5aaf62721a9f5a40e
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-11 10:48:25 -0300

    Add files via upload
    Altz

---
+++ caracteres/TestaCaracteres.java
@@ -0,0 +1,26 @@
+package caracteres;
+
+public class TestaCaracteres {
+	
+	public static void main(String[]args) {
+		//guarda um unico caractere e só pode ser quardado por '' ou por numero mais cada numero vai ser um sinbulo especifico
+		char sinbuli = 22322;
+		System.out.println(sinbuli);
+		char letra = 's';
+		System.out.println(letra);
+		
+		char valor = 65;
+		System.out.println(valor);
+		
+		valor = (char) (valor+1);
+		System.out.println(valor);
+		
+		String palavra="JairAssis";
+		System.out.println(palavra );
+		
+		palavra=palavra+"Dev";
+		System.out.println(palavra);
+		
+
+}
+	}
