f65072328b1cd4bc10ecd53958040e774c74446f
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-11 11:24:03 -0300

    Add files via upload
    atllz

---
+++ lacodeRepeticao/lacoFor.java
@@ -0,0 +1,9 @@
+package lacodeRepeticao;
+
+public class lacoFor {
+	public static void main(String[] args) {
+		for(int contador=1; contador<=10;contador++) {
+			System.out.println(contador);
+		}
+	}
+}
